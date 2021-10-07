288488e213001d9dd7ca9f824757b103f50c3455
pig1 <redsky21@lgcns.com>
2021-10-07 20:55:59 +0900

    add: button

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  const [firstState,setFirstState]=useState<string>();
+    const [firstState, setFirstState] = useState<string>()
 
-  useEffect(()=>{
-    setFirstState('가냐');
-  },[]);
+    useEffect(() => {
+        setFirstState('가냐')
+    }, [])
 
-  return (
-    <div className="App">
-      <h1>사냥을 시작해볼까</h1>
-      <h2>{firstState}</h2>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>사냥을 시작해볼까</h1>
+            <h2>{firstState}</h2>
+            <button>안녕</button>
+        </div>
+    )
 }
 
-export default App;
+export default App
