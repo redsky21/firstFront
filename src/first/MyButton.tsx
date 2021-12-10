33f049fe87afdd6e955e02e6577ce743c1693053
pig1 <redsky21@lgcns.com>
2021-12-10 13:11:05 +0900

    style component 추가

---
+++ src/first/MyButton.tsx
@@ -0,0 +1,27 @@
+import { Button, styled } from '@mui/material';
+import React from 'react';
+import { ButtonProps } from '@mui/material';
+
+export type TButtonProps = {
+  myType?: string;
+} & ButtonProps;
+
+const MyButton = styled(Button)({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+});
+
+// export default function StyledComponents() {
+//   return <MyButton>Styled Components</MyButton>;
+// }
+const StyledComponents = React.forwardRef<HTMLDivElement, TButtonProps>(
+  ({ color = 'primary', className, children, ...rest }, ref) => {
+    return <MyButton {...rest}>{children}</MyButton>;
+  },
+);
+export default StyledComponents;
