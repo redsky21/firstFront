0c48371d87308d7a6a6e497a0e0cb49d7974e15a
sweet xoju <sweetxoju@gmail.com>
2021-12-16 17:18:43 +0900

    props 추가

--- src/first/MyButton.tsx
+++ src/first/MyButton.tsx
@@ -6,22 +6,23 @@ export type TButtonProps = {
   myType?: string;
 } & ButtonProps;
 
-const MyButton = styled(Button)({
+const MyButton = styled(Button)((props:TButtonProps)=>({
   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   border: 0,
   borderRadius: 3,
   boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  color: 'white',
+  color: (props.myType==='gg'?'red':'green'),
   height: 48,
   padding: '0 30px',
-});
+
+}));
 
 // export default function StyledComponents() {
 //   return <MyButton>Styled Components</MyButton>;
 // }
 const StyledComponents = React.forwardRef<HTMLDivElement, TButtonProps>(
   ({ color = 'primary', className, children, ...rest }, ref) => {
-    return <MyButton {...rest}>{children}</MyButton>;
+    return <MyButton  {...rest}>{children}</MyButton>;
   },
 );
 export default StyledComponents;
