89a4e9f5182e59b2db6efe5789f2628750c58cfc
sweetxoju <sweetxoju@gmail.com>
2022-01-04 13:39:17 +0900

    ㅇㅇ

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,18 +1,19 @@
-import React from "react";
-import { Grid } from "@mui/material";
-import Top from "./Top";
-const Layout = () =>{
-    return <Grid container spacing={2}>
-        <Grid item xs={12}>
-            <Top/>
-        </Grid>
-        <Grid item xs={4}>
-            <div>left</div>
-        </Grid>
-        <Grid item xs={8}>
-            <div>main</div>
-        </Grid>
-        
-    </Grid>;
-}
+import React from 'react';
+import { Grid } from '@mui/material';
+import Top from './Top';
+const Layout = () => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Top />
+      </Grid>
+      <Grid item xs={4}>
+        <div>left</div>
+      </Grid>
+      <Grid item xs={8}>
+        <div>main</div>
+      </Grid>
+    </Grid>
+  );
+};
 export default Layout;
